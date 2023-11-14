e4648313aa2e8ee5cfed973d81cec10ecb0c6960
Prabuddha Chatterjee <prabuddhapiku@gmail.com>
2023-11-14 16:53:36 +0530

    changed the loading screen

--- app/sell_rent/loading.jsx
+++ app/sell_rent/loading.jsx
@@ -4,7 +4,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 
 export default function Loading() {
     return (
-        <div className='h-screen w-full sm:bg-img-mob lg:bg-img'>
+        <div className='h-screen w-full bg-white'>
             <Box sx={{ width: '100%' }}>
                 <LinearProgress />
             </Box>
